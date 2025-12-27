6a0e668bde4c6027652d8ff55d9aebdd64098ce8
Peter McGowan <peteburquette@gmail.com>
2025-12-27 00:17:52 +0000

    Upgrade to RN 0.83, new splash

---
+++ client/android/app/src/main/java/com/buachaillmaith/healthycalc/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.buachaillmaith.healthycalc
+
+import com.facebook.react.ReactActivity
+import com.facebook.react.ReactActivityDelegate
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
+import com.facebook.react.defaults.DefaultReactActivityDelegate
+// splash screen
+import android.os.Bundle
+import com.zoontek.rnbootsplash.RNBootSplash
+
+
+class MainActivity : ReactActivity() {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  override fun getMainComponentName(): String = "Healthy AI"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // splash screen
+        RNBootSplash.init(this, R.style.BootTheme)
+        super.onCreate(savedInstanceState)
+    }
+  /**
+   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
+   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
+   */
+  override fun createReactActivityDelegate(): ReactActivityDelegate =
+      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+}
